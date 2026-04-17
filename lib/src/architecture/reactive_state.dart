5c83cdf8e67f601ac79d2d7f6eb628143923bf6c
Maximiliano Camilo <MaxiCamilo@github.com>
2026-04-17 17:42:34 -0300

    refactor: Rename LifeCoordinator to LifecycleScope for consistency in reactive state management

--- lib/src/architecture/reactive_state.dart
+++ lib/src/architecture/reactive_state.dart
@@ -2,28 +2,26 @@ import 'package:flutter/widgets.dart';
 import 'package:maxi_framework/maxi_framework.dart';
 
 mixin ReactiveState<T extends StatefulWidget> on State<T> {
-  late final LifeCoordinator? _heart;
+  late final LifecycleScope? _heart;
 
-  LifeCoordinator get heart {
+  LifecycleScope get heart {
     if (_heart == null) {
       throw NegativeResult(
         error: ControlledFailure(
           errorCode: ErrorCode.implementationFailure,
-          message: FixedOration(message: 'LifeCoordinator has not been initialized yet. Make sure to call super.initState() in your initState method.'),
+          message: FixedOration(message: 'LifecycleScope has not been initialized yet. Make sure to call super.initState() in your initState method.'),
         ),
       );
     }
     return _heart;
   }
 
-   
-
   @override
   @mustCallSuper
   void initState() {
     super.initState();
 
-    _heart = LifeCoordinator();
+    _heart = LifecycleScope();
   }
 
   @override
